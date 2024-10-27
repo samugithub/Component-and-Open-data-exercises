66e40b3e118d8d7efe4a4a878d0277e6d3c825fc
samugithub <samuelhy@hotmail.fi>
2024-10-27 21:30:52 +0200

    Updates

--- cat-facts/src/CatFacts.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function CatFact({ fact }) {
-  return <p>{fact || 'Loading...'}</p>;
-}
-
-export default CatFact;
--- cat-facts/src/FetchButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function FetchButton({ onClick }) {
-  return <button onClick={onClick}>Get Another Fact</button>;
-}
-
-export default FetchButton;
